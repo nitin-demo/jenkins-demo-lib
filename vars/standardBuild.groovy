34a42294fabd48781002bafe6f423cc9b717c5f8
Hemant Kumar <hemant_4000@yahoo.in>
2019-04-16 16:09:58 +0530

    Update standardBuild.groovy

--- vars/standardBuild.groovy
+++ vars/standardBuild.groovy
@@ -1,15 +1,33 @@
 def call(Map config) {
     node {
+        stage('Cleaning Up Workspace'){
+            echo "Cleaning up ${WORKSPACE}"
+            // clean up our workspace 
+           deleteDir()
+            // clean up tmp directory 
+            dir("${workspace}@tmp") {
+                deleteDir()            
+            }   
+            
+        }
         stage('Checkout') {
+            echo "cheking out in ${WORKSPACE}"
             checkout scm
         }
-        stage('Main') {
-            docker.image(config.environment).inside {
-                sh config.mainScript
-            }
+        stage('Compile') {
+            echo "Compiling in ${WORKSPACE}"
+            sh config.compile
+        }
+        stage('Test') {
+            echo "Runing test in ${WORKSPACE}"
+            sh config.test
+        }
+        stage ('Package'){
+            echo "Building snapshot in ${WORKSPACE}"
+        sh config.package
         }
         stage('Post') {
-            sh config.postScript
-        }
-    }
+            echo "Successfully built for ${config.environment}"
+      }
+   }
 }
