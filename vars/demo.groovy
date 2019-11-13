eee04d94e7389fdf673caf5a1e25510e4faf77f1
Prashant Vats <pras.vats@gmail.com>
2019-11-13 13:13:13 +0530

    kss

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -1,10 +1,12 @@
+import com.kss.builder.Utils
+
 def call() {
     return Math.abs(new Random().nextInt() % 8000) + 1000
 }
 
 
 def getcommit(){
-    def Utils = new com.kss.builder.Utils()
+    def Utils = new Utils()
     def value = Utils.lastCommit()
     return value
 }
